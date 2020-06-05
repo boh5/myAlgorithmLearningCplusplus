5ae4272822158d3a0e03c472b281476c36d711bc
dilless <bohuang1994@gmail.com>
2020-06-05 15:12:11 +0800

    merge sort

--- main.cpp
+++ main.cpp
@@ -27,20 +27,66 @@ void insertionSort(T arr[], int n) {
 //        }
         T e = arr[i];
         int j;
-        for (j = i;  j > 0 && arr[j-1] > e ; --j) {
+        for (j = i; j > 0 && arr[j - 1] > e; --j) {
             arr[j] = arr[j - 1];
         }
         arr[j] = e;
     }
 }
 
+template<typename T>
+void __merge(T arr[], int l, int mid, int r) {
+    T aux[r - l + 1];
+    for (int i = l; i <= r; ++i) {
+        aux[i - l] = arr[i];
+    }
+    int i = l, j = mid + 1;
+    for (int k = l; k <= r; ++k) {
+        if (i > mid) {
+            arr[k] = aux[j - l];
+            j++;
+        } else if (j > r) {
+            arr[k] = aux[i - l];
+            i++;
+        } else {
+            if (aux[i - l] < aux[j - l]) {
+                arr[k] = aux[i - l];
+                i++;
+            } else {
+                arr[k] = aux[j - l];
+                j++;
+            }
+        }
+    }
+}
+
+template<typename T>
+void __mergeSort(T arr[], int l, int r) {
+
+    if (l >= r) {
+        return;
+    }
+
+    int mid = (l + r) / 2;
+    __mergeSort(arr, l, mid);
+    __mergeSort(arr, mid + 1, r);
+
+    __merge(arr, l, mid, r);
+}
+
+// 归并排序
+template<typename T>
+void mergeSort(T arr[], int n) {
+    __mergeSort(arr, 0, n - 1);
+}
+
 
 int main() {
-    int n = 10000;
-    int *arr = SortTestHelper::generateNearlyOrderedArray(n, 100);
+    int n = 50000;
+    int *arr = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr, n);
-    SortTestHelper::testSort("Selection Sort", selectionSort, arr, n);
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
     delete[] arr;
     delete[] arr2;
 
