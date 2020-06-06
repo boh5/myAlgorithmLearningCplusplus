cfa4f55c6bb6f4b11f7bc5eeb4ee1bea7d79d976
dilless <bohuang1994@gmail.com>
2020-06-06 11:10:26 +0800

    quick sort 3 ways

--- main.cpp
+++ main.cpp
@@ -197,39 +197,83 @@ void quickSort2(T arr[], int n) {
     __quickSort2(arr, 0, n - 1);
 }
 
+template<typename T>
+void __quickSort3Ways(T arr[], int l, int r) {
+    if (r - l <= 15) {
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    // partition
+    swap(arr[l], arr[rand() % (r - l + 1) + l]);
+    T v = arr[l];
+
+    int lt = l; // arr[l+1, lt] < v
+    int gt = r + 1; // arr[gt,r] > v
+    int i = l + 1; // arr[lt+1, i) == v
+    while (i < gt) {
+        if (arr[i] < v) {
+            swap(arr[i], arr[lt + 1]);
+            lt++;
+            i++;
+        } else if (arr[i] > v) {
+            swap(arr[i], arr[gt - 1]);
+            gt--;
+        } else {
+            i++;
+        }
+    }
+    swap(arr[l], arr[lt]);
+
+    __quickSort3Ways(arr, l, lt - 1);
+    __quickSort3Ways(arr, gt, r);
+}
+
+// 三路快排
+template<typename T>
+void quickSort3Ways(T arr[], int n) {
+    srand(time(NULL));
+    __quickSort3Ways(arr, 0, n - 1);
+}
+
 int main() {
     int n = 1000000;
     int *arr1 = SortTestHelper::generateRandomArray(n, 0, n);
     int *arr2 = SortTestHelper::copyIntArray(arr1, n);
     int *arr3 = SortTestHelper::copyIntArray(arr1, n);
-    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-    SortTestHelper::testSort("Quick Sort2", quickSort2, arr3, n);
+    SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
+    cout<<endl;
 
     delete[] arr1;
     delete[] arr2;
+    delete[] arr3;
 
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, 100);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
     arr3 = SortTestHelper::copyIntArray(arr1, n);
-    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-    SortTestHelper::testSort("Quick Sort2", quickSort2, arr3, n);
+    SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
+    cout<<endl;
 
     delete[] arr1;
     delete[] arr2;
+    delete[] arr3;
 
     arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
     arr3 = SortTestHelper::copyIntArray(arr1, n);
 
-    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
-//    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
-    SortTestHelper::testSort("Quick Sort2", quickSort2, arr3, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
+    SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
+    cout<<endl;
 
     delete[] arr1;
     delete[] arr2;
-
+    delete[] arr3;
 
     return 0;
 }
