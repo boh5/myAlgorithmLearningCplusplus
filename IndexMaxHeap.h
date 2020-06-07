70c2e35ee16cc399573f85e2baac3498de5fb976
dilless <bohuang1994@gmail.com>
2020-06-07 22:42:02 +0800

    optimize index max heap

--- IndexMaxHeap.h
+++ IndexMaxHeap.h
@@ -17,7 +17,9 @@ class IndexMaxHeap {
 private:
     Item *data;
 
-    int *indexes{};
+    int *indexes;
+    int *reverse;
+
     int count;
     int capacity{};
 
@@ -25,6 +27,8 @@ private:
 
         while (k > 1 && data[indexes[k / 2]] < data[indexes[k]]) {
             swap(indexes[k / 2], indexes[k]);
+            reverse[indexes[k / 2]] = k / 2;
+            reverse[indexes[k]] = k;
             k /= 2;
         }
     }
@@ -42,6 +46,8 @@ private:
                 break;
             }
             swap(indexes[k], indexes[j]);
+            reverse[indexes[k]] = k;
+            reverse[indexes[j]] = j;
             k = j;
         }
     }
@@ -49,6 +55,11 @@ private:
 public:
     explicit IndexMaxHeap(int capacity) {
         data = new Item[capacity + 1];
+        indexes = new int[capacity + 1];
+        reverse = new int[capacity + 1];
+        for (int i = 0; i < capacity; ++i) {
+            reverse[i] = 0;
+        }
         count = 0;
         this->capacity = capacity;
     }
@@ -56,6 +67,7 @@ public:
     ~IndexMaxHeap() {
         delete[] data;
         delete[] indexes;
+        delete[] reverse;
     }
 
     int size() {
@@ -75,6 +87,7 @@ public:
         i++;
         data[i] = item;
         indexes[count + 1] = i;
+        reverse[i] = count + 1;
         count++;
         shiftUp(count);
     }
@@ -86,6 +99,8 @@ public:
         Item ret = data[indexes[1]];
 
         swap(indexes[1], indexes[count]);
+        reverse[indexes[1]] = 1;
+        reverse[indexes[count]] = 0;
         count--;
         shiftDown(1);
 
@@ -99,30 +114,45 @@ public:
         int ret = indexes[1] - 1;
 
         swap(indexes[1], indexes[count]);
+        reverse[indexes[1]] = 1;
+        reverse[indexes[count]] = 0;
         count--;
         shiftDown(1);
 
         return ret;
     }
 
+    bool contain(int i) {
+        assert(i + 1 >= 1 && i + 1 <= capacity);
+        return reverse[i + 1] != 0;
+    }
+
     Item getItem(int i) {
+
+        assert(contain(i));
+
         return data[i + 1];
     }
 
     void change(int i, Item newItem) {
+
+        assert(contain(i));
 
         i += 1;
         data[i] = newItem;
 
         // 找到 indexes[j] = i, j 表示 data[i] 在堆中的位置
         // 之后 shiftUp(j), 在 shiftDown(j)
-        for (int j = 1; j <= count; ++j) {
-            if (indexes[j] == i) {
-                shiftUp(j);
-                shiftDown(j);
-                return;
-            }
-        }
+//        for (int j = 1; j <= count; ++j) {
+//            if (indexes[j] == i) {
+//                shiftUp(j);
+//                shiftDown(j);
+//                return;
+//            }
+//        }
+        int j = reverse[i];
+        shiftUp(j);
+        shiftDown(j);
     }
 
 public:
