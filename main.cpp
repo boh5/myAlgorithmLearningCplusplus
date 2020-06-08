4bd2cee3cac72699f61cce64f6f9571066f1c39b
dilless <bohuang1994@gmail.com>
2020-06-08 10:11:26 +0800

    binary search

--- main.cpp
+++ main.cpp
@@ -6,10 +6,9 @@
 #include "Sort/QuickSort2Ways.h"
 #include "Sort/QuickSort3Ways.h"
 #include "Sort/MergeSort.h"
+#include "BinarySearchTree/BinarySearch.h"
 
 using namespace std;
-
-
 
 
 int main() {
@@ -20,11 +19,10 @@ int main() {
     SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
     SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
-    cout<<endl;
-
-    delete[] arr1;
-    delete[] arr2;
-    delete[] arr3;
+    cout << endl;
+    cout << binarySearch(arr1, n, 10000) << endl;
+    cout << binarySearch2(arr1, n, 10000) << endl;
+    cout << endl;
 
     arr1 = SortTestHelper::generateNearlyOrderedArray(n, 100);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
@@ -32,11 +30,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
     SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
-    cout<<endl;
-
-    delete[] arr1;
-    delete[] arr2;
-    delete[] arr3;
+    cout << endl;
 
     arr1 = SortTestHelper::generateRandomArray(n, 0, 10);
     arr2 = SortTestHelper::copyIntArray(arr1, n);
@@ -45,7 +39,7 @@ int main() {
 //    SortTestHelper::testSort("Quick Sort", quickSort, arr1, n);
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr2, n);
     SortTestHelper::testSort("Quick Sort 3Ways", quickSort3Ways, arr3, n);
-    cout<<endl;
+    cout << endl;
 
     delete[] arr1;
     delete[] arr2;
