193a503b974f37d858a6cf920cc206f74d9dfa2f
dilless <bohuang1994@gmail.com>
2020-06-09 17:25:57 +0800

    disadvantage of BST

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <algorithm>
 #include "BinarySearch.h"
 #include "BST.h"
 #include "FileOps.h"
@@ -11,7 +12,8 @@ using namespace std;
 
 int main() {
 
-    string filename = "D:\\Files\\codes\\cplusplus\\algorithm\\BinarySearchTree\\bible.txt";
+//    string filename = "D:\\Files\\codes\\cplusplus\\algorithm\\BinarySearchTree\\bible.txt";
+    string filename = "D:\\Files\\codes\\cplusplus\\algorithm\\BinarySearchTree\\communist.txt";
     vector<string> words;
 
     if (FileOps::readFile(filename, words)) {
@@ -29,25 +31,42 @@ int main() {
                 (*res)++;
             }
         }
-        cout << "'god': " << *bst.search("god") << endl;
+        cout << "'unite': " << *bst.search("unite") << endl;
         time_t endTime = clock();
         cout << "BST, time: " << double(endTime - startTime) / CLOCKS_PER_SEC;
         cout << endl;
 
         // test SST
         startTime = clock();
-        SequenceST<string, int> sst = SequenceST<string, int>();
+        SequenceST<string, int> *sst = new SequenceST<string, int>();
         for (vector<string>::iterator iter = words.begin(); iter < words.end(); iter++) {
-            int *res = sst.search(*iter);
+            int *res = (*sst).search(*iter);
             if (res == NULL) {
-                bst.insert(*iter, 1);
+                (*sst).insert(*iter, 1);
             } else {
                 (*res)++;
             }
         }
-        cout << "'god': " << *sst.search("god") << endl;
+        cout << "'unite': " << *(*sst).search("unite") << endl;
         endTime = clock();
         cout << "SST, time: " << double(endTime - startTime) / CLOCKS_PER_SEC;
+        cout << endl;
+
+        // test BST2
+        sort(words.begin(), words.end());
+        startTime = clock();
+        BST<string, int> *bst2 = new BST<string, int>();
+        for (vector<string>::iterator iter = words.begin(); iter < words.end(); iter++) {
+            int *res = (*bst2).search(*iter);
+            if (res == NULL) {
+                (*bst2).insert(*iter, 1);
+            } else {
+                (*res)++;
+            }
+        }
+        cout << "'unite': " << *(*bst2).search("unite") << endl;
+        endTime = clock();
+        cout << "BST2, time: " << double(endTime - startTime) / CLOCKS_PER_SEC;
         cout << endl;
     }
 
