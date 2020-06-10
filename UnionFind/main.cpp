9e5b78e14932784a0fb715b2d3236cd67b9beb60
dilless <bohuang1994@gmail.com>
2020-06-10 14:30:01 +0800

    union find 4 rank

--- UnionFind/main.cpp
+++ UnionFind/main.cpp
@@ -5,11 +5,12 @@
 
 int main() {
 
-    int n = 10000;
+    int n = 1000000;
 
-    UnionFindTestHelper::testUF1(n);
-    UnionFindTestHelper::testUF2(n);
+//    UnionFindTestHelper::testUF1(n);
+//    UnionFindTestHelper::testUF2(n);
     UnionFindTestHelper::testUF3(n);
+    UnionFindTestHelper::testUF4(n);
 
     return 0;
 }
