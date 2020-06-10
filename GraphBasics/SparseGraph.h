8fd24197cb596e1c95700aa0be690404a9a2628a
dilless <bohuang1994@gmail.com>
2020-06-10 17:22:23 +0800

    graph iterator

--- GraphBasics/SparseGraph.h
+++ GraphBasics/SparseGraph.h
@@ -22,7 +22,7 @@ public:
         this->n = n;
         this->m = 0;
         this->directed = directed;
-        for (int i = 0; i <; ++i) {
+        for (int i = 0; i < n; ++i) {
             g.push_back(vector<int>());
         }
     }
@@ -56,6 +56,38 @@ public:
             return g[v][i] == w;
         }
     }
+
+    class adjIterator {
+    private:
+        SparseGraph &G;
+        int v;
+        int index;
+    public:
+        adjIterator(SparseGraph &graph, int v) : G(graph) {
+            this->v = v;
+            this->index = 0;
+        }
+
+        int begin() {
+            index = 0;
+            if (G.g[v].size()) {
+                return G.g[v][index];
+            }
+            return -1;
+        }
+
+        int next() {
+            index++;
+            if (index < G.g[v].size()) {
+                return G.g[v][index];
+            }
+            return -1;
+        }
+
+        bool end() {
+            return index >= G.g[v].size();
+        }
+    };
 
 };
 
