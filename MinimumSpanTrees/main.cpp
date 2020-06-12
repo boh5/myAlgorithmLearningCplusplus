eae86626b132fb8cf1f49050f998418ba57da13a
dilless <bohuang1994@gmail.com>
2020-06-12 16:29:25 +0800

    lazy prim mst

--- MinimumSpanTrees/main.cpp
+++ MinimumSpanTrees/main.cpp
@@ -6,21 +6,26 @@
 #include "DenseGraph.h"
 #include "SparseGraph.h"
 #include "ReadGraph.h"
+#include "LazyPrimMST.h"
 
-int main () {
+int main() {
     string filename = "D:\\Files\\codes\\cplusplus\\algorithm\\MinimumSpanTrees\\testG1.txt";
     int V = 8;
     cout << fixed << setprecision(2);
 
-    DenseGraph<double> g1 = DenseGraph<double>(V, false);
-    ReadGraph<DenseGraph<double>, double> readGraph1(g1, filename);
-    g1.show();
-    cout<<endl;
+    SparseGraph<double> g = SparseGraph<double>(V, false);
+    ReadGraph<SparseGraph<double>, double> readGraph2(g, filename);
+    g.show();
+    cout << endl;
 
-    SparseGraph<double> g2 = SparseGraph<double>(V, false);
-    ReadGraph<SparseGraph<double>, double> readGraph2(g2, filename);
-    g2.show();
-    cout<<endl;
+    cout << "Test Lazy Prim MST:" << endl;
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+    vector<Edge<double>> mst = lazyPrimMST.mstEdges();
+    for (int i = 0; i < mst.size(); ++i) {
+        cout << mst[i] << endl;
+    }
+    cout << "The MST weight is: " << lazyPrimMST.result() << endl;
+    cout << endl;
 
     return 0;
 }
